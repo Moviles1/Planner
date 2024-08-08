36852b8db54fee7b6f9627d187de7972bb1ede27
Sutmi <sutmiramirez03@micorreo.upp.edu.mx>
2024-08-07 19:20:41 -0600

    ci

--- app/src/main/java/com/example/lunaplanner/MainActivity.java
+++ app/src/main/java/com/example/lunaplanner/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_main);
 
         Btn_Login = findViewById(R.id.Btn_Login);
