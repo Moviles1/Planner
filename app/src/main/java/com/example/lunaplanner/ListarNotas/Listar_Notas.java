b7fdb442da38bc39bc4973ffb7212689aff53f6f
Sutmi <sutmiramirez03@micorreo.upp.edu.mx>
2024-08-13 22:21:31 -0600

    Mas cambios

--- app/src/main/java/com/example/lunaplanner/ListarNotas/Listar_Notas.java
+++ app/src/main/java/com/example/lunaplanner/ListarNotas/Listar_Notas.java
@@ -1,26 +1,200 @@
 package com.example.lunaplanner.ListarNotas;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.lunaplanner.Objetos.Nota;
 import com.example.lunaplanner.R;
+import com.example.lunaplanner.ViewHolder.ViewHolder_nota;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class Listar_Notas extends AppCompatActivity {
+
+    RecyclerView recyclerviewNotas;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference BASE_DE_DATOS;
+
+    LinearLayoutManager linearLayoutManager;
+    FirebaseRecyclerAdapter<Nota, ViewHolder_nota> firebaseRecyclerAdapter;
+    FirebaseRecyclerOptions<Nota> options;
+
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_listar_notas);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("Mis Notas");
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recyclerviewNotas = findViewById(R.id.reciclerviewNotas);
+        recyclerviewNotas.setHasFixedSize(true);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        BASE_DE_DATOS = firebaseDatabase.getReference("Notas_Publicadas");
+        dialog = new Dialog(Listar_Notas.this);
+        ListarNotasUsuarios();
+     }
+
+     private void ListarNotasUsuarios(){
+        options = new FirebaseRecyclerOptions.Builder<Nota>().setQuery(BASE_DE_DATOS, Nota.class).build();
+        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Nota, ViewHolder_nota>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ViewHolder_nota viewHolder_nota, int position, @NonNull Nota nota) {
+                viewHolder_nota.SetearDatos(
+                       getApplicationContext(),
+                        nota.getId_nota(),
+                        nota.getUid_usuario(),
+                        nota.getCorreo_usuario(),
+                        nota.getFecha_hora_actual(),
+                        nota.getTitulo(),
+                        nota.getDescripcion(),
+                        nota.getFecha_nota(),
+                        nota.getEstado()
+                );
+            }
+
+
+            @Override
+            public ViewHolder_nota onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_nota,parent,false);
+                ViewHolder_nota viewHolder_nota = new ViewHolder_nota(view);
+
+                viewHolder_nota.setOnClickListener(new ViewHolder_nota.ClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        Toast.makeText(Listar_Notas.this, "on item click", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onItemLongClick(View view, int position) {
+
+                        String id_nota = getItem(position).getId_nota();
+                    //Declara las vistas
+                        Button CD_Eliminar, CD_Actualizar;
+                        
+                        //Realizar la conexion con el diseño
+                        dialog.setContentView(R.layout.dialogo_opciones);
+                        
+                        //Inicializar las vistas
+                        CD_Eliminar = dialog.findViewById(R.id.CD_Eliminar);
+                        CD_Actualizar = dialog.findViewById(R.id.CD_Actualizar);
+                        
+                        CD_Eliminar.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                EliminarNota(id_nota);
+                                dialog.dismiss();
+                            }
+                        });
+                    CD_Actualizar.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Toast.makeText(Listar_Notas.this, "Actualizar nota", Toast.LENGTH_SHORT).show();
+                            dialog.dismiss();
+                        }
+                    });
+                    dialog.show();
+                    }
+                });
+                return viewHolder_nota;
+            }
+        };
+
+        linearLayoutManager = new LinearLayoutManager(Listar_Notas.this, LinearLayoutManager.VERTICAL, false);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+
+        recyclerviewNotas.setLayoutManager(linearLayoutManager);
+        recyclerviewNotas.setAdapter(firebaseRecyclerAdapter);
+
+
+     }
+
+    private void EliminarNota(String idNota) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(Listar_Notas.this);
+        builder.setTitle("Eliminar nota");
+        builder.setMessage("¿Desea eliminar la nota?");
+        builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+           //Eliminar Nota en BD
+                Query query = BASE_DE_DATOS.orderByChild("id_nota").equalTo(idNota);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot ds : snapshot.getChildren()){
+                            ds.getRef().removeValue();
+                        }
+                        Toast.makeText(Listar_Notas.this, "Nota Eliminada", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(Listar_Notas.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(Listar_Notas.this, "Cancelado por el usuario", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.create().show();
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (firebaseRecyclerAdapter != null) {
+            firebaseRecyclerAdapter.startListening();
+        }
+
+    }
+    @Override
+public boolean onSupportNavigateUp() {
+    onBackPressed();
+    return super.onSupportNavigateUp();
+}
 }
